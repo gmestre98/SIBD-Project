cac49ec6a31b156878361b8c3565a00db5514f2b
gmestre98 <goncalo.mestre@me.com>
2019-11-08 18:44:58 +0000

    Add files via upload
    Query 5 after review

--- query5.sql
+++ query5.sql
@@ -1,21 +1,5 @@
-
-/** ------------------------------------------- 5 ------------------------------------------**/
-/** 5. For each possible diagnosis, presenting the code together with the
-description, list the number of distinct medication names that have
-been prescribed to treat that condition. Sort the results according
-to the number of distinct medication names, in ascending order.**/
-
-SELECT DISTINCT diagnostic_code.ID, diagnostic_code.description, COUNT(DISTINCT prescription.name) AS n_medication
-FROM diagnostic_code
-LEFT JOIN consultation_diagnostic
-USING (ID)
-LEFT JOIN prescription
-USING (ID)
-GROUP BY diagnostic_code.ID
-ORDER BY COUNT(DISTINCT prescription.name);
-
-SELECT DISTINCT d.ID, d.description, COUNT(DISTINCT p.name) AS number_medication
-FROM diagnostic_code d, prescription p
-WHERE d.ID = p.ID
+SELECT DISTINCT COUNT(DISTINCT p.name) AS medication_usage, d.ID, d.description 
+FROM diagnostic_code AS d LEFT OUTER JOIN prescription AS p
+ON d.ID = p.ID
 GROUP BY d.ID
 ORDER BY COUNT(DISTINCT p.name) ASC;
