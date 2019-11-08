46088bfa18c2b588d5acf571c8daf42ecb45bcbe
gmestre98 <goncalo.mestre@me.com>
2019-11-08 19:40:19 +0000

    Add files via upload

--- query9.sql
+++ query9.sql
@@ -2,12 +2,14 @@ SELECT app.name, app.street, app.city, app.zip
 FROM (SELECT name, street, city, zip, COUNT(a.date_timestamp) AS count_a
     FROM client AS c, appointment AS a
     WHERE a.VAT_client = c.VAT
+    AND a.date_timestamp LIKE '2019%'
     GROUP BY c.VAT) AS app
 JOIN
     (SELECT name, street, city, zip, COUNT(con.date_timestamp) AS count_b
     FROM client AS c, appointment AS a, consultation AS con
     WHERE a.VAT_client = c.VAT
     AND a.VAT_doctor = con.VAT_doctor
+    AND a.date_timestamp LIKE '2019%'
     AND a.date_timestamp = con.date_timestamp
     GROUP BY c.VAT) co
 ON app.count_a = co.count_b
