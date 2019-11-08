f69d89f21e6c23902421bbe579fc03ca653c2605
gmestre98 <goncalo.mestre@me.com>
2019-11-08 21:44:05 +0000

    Add files via upload

--- query3.sql
+++ query3.sql
@@ -5,4 +5,4 @@ AND a1.VAT_doctor = c.VAT_doctor
 AND a1.date_timestamp = c.date_timestamp
 AND (SOAP_O LIKE '%gingivitis%' OR SOAP_O LIKE '%periodontitis%')
 AND c.date_timestamp = (SELECT MAX(a2.date_timestamp) FROM appointment a2 WHERE a1.VAT_client = a2.VAT_client)
-ORDER BY name ASC;
+ORDER BY name;
