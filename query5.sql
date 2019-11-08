57410b267c1746796191f95b63215fe1faebee0c
gmestre98 <goncalo.mestre@me.com>
2019-11-08 22:09:34 +0000

    Add files via upload

--- query5.sql
+++ query5.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT COUNT(DISTINCT p.name) AS medication_usage, d.ID, d.description 
+SELECT COUNT(DISTINCT p.name) AS medication_usage, d.ID, d.description 
 FROM diagnostic_code AS d LEFT OUTER JOIN prescription AS p
 ON d.ID = p.ID
 GROUP BY d.ID
-ORDER BY COUNT(DISTINCT p.name) ASC;
+ORDER BY COUNT(DISTINCT p.name);
