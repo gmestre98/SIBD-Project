3b9764140508389bab953d07d9ec6c9966bc8a4f
gmestre98 <goncalo.mestre@me.com>
2019-11-08 18:11:32 +0000

    Add files via upload
    Query 4 after review

--- query4.sql
+++ query4.sql
@@ -1,12 +1,7 @@
-
-/** ------------------------------------------- 4 ------------------------------------------**/
-/** 4. List the name, VAT and address (i.e., street, city and zip) of all
-clients of the clinic that have had appointments but that never had
-a consultation (i.e., clients that never showed to an appointment).**/
-
 SELECT DISTINCT name, VAT, street, city, zip
-FROM client, appointment
+FROM client
 WHERE VAT IN (SELECT a.VAT_client
-	FROM appointment a NATURAL LEFT OUTER JOIN consultation c
+	FROM appointment a LEFT OUTER JOIN consultation c
+	ON a.VAT_doctor = c.VAT_doctor AND a.date_timestamp = c.date_timestamp
 	GROUP BY VAT_client
 	HAVING COUNT(c.date_timestamp) = 0);
