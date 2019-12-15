808d907f2f0e844a004c96f61afe0505855c80e9
gmestre98 <goncalo.mestre@tecnico.ulisboa.pt>
2019-12-15 21:22:50 +0000

    Delete query5.sql

--- query5.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(DISTINCT p.name) AS medication_usage, d.ID, d.description 
-FROM diagnostic_code AS d LEFT OUTER JOIN prescription AS p
-ON d.ID = p.ID
-GROUP BY d.ID
-ORDER BY COUNT(DISTINCT p.name);
