510cb05cda49935de558c79c393a2ff6fa9a161f
gmestre98 <goncalo.mestre@tecnico.ulisboa.pt>
2019-12-15 21:23:26 +0000

    Delete update2.sql

--- update2.sql
+++
@@ -1,8 +0,0 @@
-
-UPDATE employee e
-SET salary = 1.05*salary
-WHERE(SELECT COUNT(*)
-	FROM appointment a
-	WHERE e.VAT = a.VAT_doctor
-	AND YEAR(a.date_timestamp)=2019
-	GROUP BY VAT_doctor) > 100;
